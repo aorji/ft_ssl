5c4051fbd29d8738c174d0e3e902b861bd201ae0
aorji <anastasia.orji@gmail.com>
2019-07-19 18:07:09 +0300

    add step 1: md5

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aorji <aorji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 13:03:37 by aorji             #+#    #+#             */
-/*   Updated: 2019/07/18 17:16:02 by aorji            ###   ########.fr       */
+/*   Updated: 2019/07/19 17:28:56 by aorji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ int main(int ac, char **av)
     if (input->error)
         return 0;
     
-    // read message
+    // read messages
     (ac > 2) ? read_message_from_file(input) : read_message_from_stdin(input);
     if (input->error)
         return 0;
 
     // run requested hashing algorithm
-    int (*hashing_algorithm[])(t_input *) = {md5, sha256};
+    int (*hashing_algorithm[])(t_input *) = { &md5, &sha256 };
     (*hashing_algorithm[input->cmd_opts - 1])(input);
 
     // system ("leaks a.out");
