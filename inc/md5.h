e5410f44509c0126ddcc72295703bf1925067ac4
aorji <anastasia.orji@gmail.com>
2019-07-26 16:08:31 +0300

    change step 3 realization

--- inc/md5.h
+++ inc/md5.h
@@ -6,7 +6,7 @@
 /*   By: aorji <aorji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/23 17:51:30 by aorji             #+#    #+#             */
-/*   Updated: 2019/07/26 13:34:34 by aorji            ###   ########.fr       */
+/*   Updated: 2019/07/26 16:07:01 by aorji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,6 @@
 
 #include "../inc/ft_ssl.h"
 
-/*
- * Constants for MD5Tr calculation_procedure
- */
-#define S11 7
-#define S12 12
-#define S13 17
-#define S14 22
-#define S21 5
-#define S22 9
-#define S23 14
-#define S24 20
-#define S31 4
-#define S32 11
-#define S33 16
-#define S34 23
-#define S41 6
-#define S42 10
-#define S43 15
-#define S44 21
 
 /* 
  * ROTATE_LEFT rotates x left n bits.
@@ -43,61 +24,45 @@
 /*
  * Basic MD5 functions.
  */
-#define F(x, y, z) (((x) & (y)) || ((!x) & (z)))
-#define G(x, y, z) (((x) & (z)) || ((y) & (!z)))
+#define F(x, y, z) (((x) & (y)) | ((!x) & (z)))
+#define G(x, y, z) (((x) & (z)) | ((y) & (!z)))
 #define H(x, y, z) ((x) ^ (y) ^ (z))
 #define I(x, y, z) ((y) ^ ((x) | (!z)))
 
 /*
- * Round 1
+ * Constants for MD5Tr calculation_procedure
  */
-#define FF(a, b, c, d, x, s, t) {             \
-        (a) += F((b), (c), (d)) + (x) + (t);  \
-        (a) = ROTATE_LEFT((a), (s));          \
-        (a) += (b);                           \
-}
-// /*
-//  * Round 2
-//  */
-#define GG(a, b, c, d, x, s, t) {             \
-        (a) += G((b), (c), (d)) + (x) + (t);  \
-        (a) = ROTATE_LEFT((a), (s));          \
-        (a) += (b);                           \
-}
-// /*
-//  * Round 3
-//  */
-#define HH(a, b, c, d, x, s, t) {             \
-        (a) += H((b), (c), (d)) + (x) + (t);  \
-        (a) = ROTATE_LEFT((a), (s));          \
-        (a) += (b);                           \
-}
-// /*
-//  * Round 4
-//  */
-#define II(a, b, c, d, x, s, t) {             \
-        (a) += I((b), (c), (d)) + (x) + (t);  \
-        (a) = ROTATE_LEFT((a), (s));          \
-        (a) += (b);                           \
-}
+static int s[] = {
+  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21
+};
 
-static const uint32_t T[] = { 0,
-    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
-    0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
-    0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
-    0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
-    0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
-    0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
-    0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
-    0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
-    0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+static int x[] = {
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+	1, 6, 11, 0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12,
+	5, 8, 11, 14, 1, 4, 7, 10, 13, 0, 3, 6, 9, 12, 15, 2,
+	0, 7, 14, 5, 12, 3, 10, 1, 8, 15, 6, 13, 4, 11, 2, 9
+};
+
+static const uint32_t T[] = {
+  0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+  0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+  0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+  0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+  0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+  0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+  0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+  0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+  0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+  0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+  0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+  0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+  0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+  0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+  0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+  0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
 };
 
 static uint8_t PADDING[] = {
@@ -109,11 +74,6 @@ static uint8_t PADDING[] = {
 /*
  * Magic constants
  */
-static int32_t A_ = 0x67452301;
-static int32_t B_ = 0xefcdab89;
-static int32_t C_ = 0x98badcfe;
-static int32_t D_ = 0x10325476;
-
 static int32_t A = 0;
 static int32_t B = 0;
 static int32_t C = 0;
@@ -140,8 +100,4 @@ static const int BIT_NUM = 8;
 // void  append_lenght(t_list **, size_t, size_t);
 // void  calculation_procedure(t_list **message);
 
-// void round_1(uint32_t X[16]);
-// void round_2(uint32_t X[16]);
-// void round_3(uint32_t X[16]);
-// void round_4(uint32_t X[16]);
 #endif
--- src/md5.c
+++ src/md5.c
@@ -6,7 +6,7 @@
 /*   By: aorji <aorji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 16:47:21 by aorji             #+#    #+#             */
-/*   Updated: 2019/07/26 13:52:13 by aorji            ###   ########.fr       */
+/*   Updated: 2019/07/26 16:07:49 by aorji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ static int padding_size(size_t message_size)
     return ( sub < a ) ? (a - sub) : (a + n - sub);
 }
 
+/*
+ * step 1
+ */
 static void append_padding(t_list **message, size_t from, size_t to)
 {
     size_t i = from;
@@ -28,6 +31,9 @@ static void append_padding(t_list **message, size_t from, size_t to)
     }
 }
 
+/*
+ * step 2
+ */
 static void append_lenght(t_list **message, size_t from, size_t len)
 {
     for(int i = 0; len != 0; ++i)
@@ -36,85 +42,13 @@ static void append_lenght(t_list **message, size_t from, size_t len)
         len = len >> 8;
     }
 }
-
-static void round_1(uint32_t X[16])
+    
+static void init_magic_num()
 {
-    FF(A, B, C, D, X[0], S11, T[1]);
-    FF(D, A, B, C, X[1], S12, T[2]);
-    FF(C, D, A, B, X[2], S13, T[3]);
-    FF(B, C, D, A, X[3], S14, T[4]);
-    FF(A, B, C, D, X[4], S11, T[5]);
-    FF(D, A, B, C, X[5], S12, T[6]);
-    FF(C, D, A, B, X[6], S13, T[7]);
-    FF(B, C, D, A, X[7], S14, T[8]);
-    FF(A, B, C, D, X[8], S11, T[9]);
-    FF(D, A, B, C, X[9], S12, T[10]);
-    FF(C, D, A, B, X[10], S13, T[11]);
-    FF(B, C, D, A, X[11], S14, T[12]);
-    FF(A, B, C, D, X[12], S11, T[13]);
-    FF(D, A, B, C, X[13], S12, T[14]);
-    FF(C, D, A, B, X[14], S13, T[15]);
-    FF(B, C, D, A, X[15], S14, T[16]); 
-}
-
-static void round_2(uint32_t X[16])
-{
-    GG(A, B, C, D, X[1], S21, T[17]);
-    GG(D, A, B, C, X[6], S22, T[18]);
-    GG(C, D, A, B, X[11], S23, T[19]);
-    GG(B, C, D, A, X[0], S24, T[20]);
-    GG(A, B, C, D, X[5], S21, T[21]);
-    GG(D, A, B, C, X[10], S22, T[22]);
-    GG(C, D, A, B, X[15], S23, T[23]);
-    GG(B, C, D, A, X[4], S24, T[24]);
-    GG(A, B, C, D, X[9], S21, T[25]);
-    GG(D, A, B, C, X[14], S22, T[26]);
-    GG(C, D, A, B, X[3], S23, T[27]);
-    GG(B, C, D, A, X[8], S24, T[28]);
-    GG(A, B, C, D, X[13], S21, T[29]);
-    GG(D, A, B, C, X[2], S22, T[30]);
-    GG(C, D, A, B, X[7], S23, T[31]);
-    GG(B, C, D, A, X[12], S24, T[32]); 
-}
-
-static void round_3(uint32_t X[16])
-{
-    HH(A, B, C, D, X[5], S31, T[33]);
-    HH(D, A, B, C, X[8], S32, T[34]);
-    HH(C, D, A, B, X[11], S33, T[35]);
-    HH(B, C, D, A, X[14], S34, T[36]);
-    HH(A, B, C, D, X[1], S31, T[37]);
-    HH(D, A, B, C, X[4], S32, T[38]);
-    HH(C, D, A, B, X[7], S33, T[39]);
-    HH(B, C, D, A, X[10], S34, T[40]);
-    HH(A, B, C, D, X[13], S31, T[41]);
-    HH(D, A, B, C, X[0], S32, T[42]);
-    HH(C, D, A, B, X[3], S33, T[43]);
-    HH(B, C, D, A, X[6], S34, T[44]);
-    HH(A, B, C, D, X[9], S31, T[45]);
-    HH(D, A, B, C, X[12], S32, T[46]);
-    HH(C, D, A, B, X[15], S33, T[47]);
-    HH(B, C, D, A, X[2], S34, T[48]);
-}
-
-static void round_4(uint32_t X[16])
-{
-    II(A, B, C, D, X[0], S41, T[49]);
-    II(D, A, B, C, X[7], S42, T[50]);
-    II(C, D, A, B, X[14], S43, T[51]);
-    II(B, C, D, A, X[5], S44, T[52]);
-    II(A, B, C, D, X[12], S41, T[53]);
-    II(D, A, B, C, X[3], S42, T[54]);
-    II(C, D, A, B, X[10], S43, T[55]);
-    II(B, C, D, A, X[1], S44, T[56]);
-    II(A, B, C, D, X[8], S41, T[57]);
-    II(D, A, B, C, X[15], S42, T[58]);
-    II(C, D, A, B, X[6], S43, T[59]);
-    II(B, C, D, A, X[13], S44, T[60]);
-    II(A, B, C, D, X[4], S41, T[61]);
-    II(D, A, B, C, X[11], S42, T[62]);
-    II(C, D, A, B, X[2], S43, T[63]);
-    II(B, C, D, A, X[9], S44, T[64]);
+    A = 0x67452301;
+    B = 0xefcdab89;
+    C = 0x98badcfe;
+    D = 0x10325476;
 }
 
 /*
@@ -122,38 +56,41 @@ static void round_4(uint32_t X[16])
  */
 static void calculation_procedure(t_list **message)
 {
-    A = A_;                                     /* init magic numbers */
-    B = B_;
-    C = C_;
-    D = D_;
+    init_magic_num();
+    uint32_t *X;                             /* storage for nth block of 16 32-bit words */
     size_t N = (*message)->content_size / 4;    /* byte_len * 8 => bit_len; bit_len / 32 => num of 32-bit words */
 
-    uint32_t X[16];                             /* storage for nth block of 16 32-bit words */
     for (size_t i = 0; i < N/16; ++i)           /* process each 16-word block. */
     {
+        X = ((*message)->content) + (i * 16);       /* copy i-th block of 16 32-bit words into X */
+
         int32_t AA = A;
         int32_t BB = B;
         int32_t CC = C;
         int32_t DD = D;
+        uint32_t res;
 
-        for (int j = 0; j < 16; ++j)            /* copy block of 16 32-bit words into X */
+        for (int i = 0; i < 64; ++i)
         {
-            X[j] = ((uint32_t *)((*message)->content))[i * 16 + j];
+            if (i < 16)
+                res = F(B, C, D);
+            else if (i < 32)
+                res = G(B, C, D);
+            else if (i < 48)
+                res = H(B, C, D);
+            else
+                res = I(B, C, D);
+            res += A + T[i] + X[x[i]];
+            A = D;
+            D = C;
+            C = B;
+            B = B + ROTATE_LEFT(res, s[i]);
         }
-        ft_printf("%x    %x  %x  %x\n", A, B, C, D);
-        round_1(X);                             /* Round 1 */
-        ft_printf("%x    %x  %x  %x\n", A, B, C, D);
-        round_2(X);                             /* Round 2 */
-        ft_printf("%x    %x  %x  %x\n", A, B, C, D);
-        round_3(X);                             /* Round 3 */
-        ft_printf("%x    %x  %x  %x\n", A, B, C, D);
-        round_4(X);                             /* Round 4 */
-        ft_printf("%x    %x  %x  %x\n", A, B, C, D);
-        
-        A = A + AA;
-        B = B + BB;
-        C = C + CC;
-        D = D + DD;
+        A += AA;
+        B += BB;
+        C += CC;
+        D += DD;
+        // ft_printf("b=%x    %x  %x  %x\n", A, B, C, D);
     }
 }
 
@@ -175,10 +112,8 @@ int         md5(t_input *input)
         append_padding(&message, message_size, message_size + padding);
         append_lenght(&message, message_size + padding, message_size * BIT_NUM);
         calculation_procedure(&message);
-        
-        // print_bitset(message->content, message->content_size);
-        // print_xset(message->content, message->content_size);/
         ft_printf("MD5 (%s) = %x    %x  %x  %x\n", input->av[i], A, B, C, D);
+
         ++i;
     }
     return 0;
